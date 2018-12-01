351145b61eacf0b20edd4943bc62581c9d87eb29
Jitendra Behera <JBehera@bridgetree.com>
2018-12-01 15:35:47 +0530

    Implement Role-Base Authorization

---
+++ src/app/_services/user.service.ts
@@ -0,0 +1,21 @@
+﻿import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../_models';
+
+
+
+@Injectable({ providedIn: 'root' })
+export class UserService {
+    apiUrl: any;
+    constructor(private http: HttpClient) { this.apiUrl = 'http://localhost:4000'; }
+
+    getAll() {
+
+        // return this.http.get<User[]>(`${config.apiUrl}/users`);http://localhost:4000
+        return this.http.get<User[]>(`${this.apiUrl}/users`)
+    }
+
+    getById(id: number) {
+        return this.http.get<User>(`${this.apiUrl}/users/${id}`);
+    }
+}
