142da11e673778a6963450bed2bddaeacc095ea4
Jitendra Behera <JBehera@bridgetree.com>
2018-12-12 12:53:06 +0530

    ffff

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,6 +10,8 @@ import { UserService, AuthenticationService } from '../_services';
 export class HomeComponent {
     currentUser: User;
     userFromApi: User;
+    declare const Twilio: any;
+  require: any;
 
     constructor(
         private userService: UserService,
@@ -18,9 +20,27 @@ export class HomeComponent {
         this.currentUser = this.authenticationService.currentUserValue;
     }
 
+    // tslint:disable-next-line:use-life-cycle-interface
     ngOnInit() {
         this.userService.getById(this.currentUser.id).pipe(first()).subscribe(user => {
             this.userFromApi = user;
         });
     }
+    clickToCall() { alert('Fired');
+
+    const accountSid = 'AC4ac4f32b3d763236620361c269232c26';
+    const authToken = 'b9942ca5a628f6bcaf793266b863a9d9';
+    const client = require('twilio')(accountSid, authToken);
+    client.calls
+          .create({
+             url: 'http://demo.twilio.com/docs/voice.xml',
+             to: '+917892855462',
+             from: '+15017122661'
+           })
+          .then(call => console.log(call.sid))
+          .done();
+
+
+
+}
 }
